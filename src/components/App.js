5ec3044b168bea595bf8ed655ca8001979e76ffa
Mark Soifer <posurlu@gmail.com>
2017-07-06 18:31:07 +0300

    upper tollbar

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import CategoryPage from './category/CategoryPage';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
+import UpperToolbar from './common/UpperToolbar';
+
 import BottomToolbar from './common/BottomToolbar';
 class App extends Component {
     render() {
         return (
             <div>
-                <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                    <AppBar title="My AppBar"/>
+                <MuiThemeProvider>
+                    <UpperToolbar/>
                 </MuiThemeProvider>
                 {this.props.children}
                 <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,45 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LocationListRow from './LocationListRow';
-import {
-    Table,
-    TableBody,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
-} from 'material-ui/Table';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import {Link} from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+const styles = {
+  gridList: {
+    height: 450,
+    backgroundSize: "contain"
+  },
+  gridTile : {  
+    width: "100%"
+}
+};
 
 const LocationList = ({locations}) => {
     return (
-        <MuiThemeProvider>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHeaderColumn>Name</TableHeaderColumn>
-                        <TableHeaderColumn>Address</TableHeaderColumn>
-                        <TableHeaderColumn>Coordinates</TableHeaderColumn>
-                        <TableHeaderColumn>Category</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {locations.map(location => 
-                    <TableRow>
-                        <TableRowColumn>
-                            <Link to={'/location/' + location.name}>{location.name}</Link>
-                        </TableRowColumn>
-                        <TableRowColumn>{location.address}</TableRowColumn>
-                        <TableRowColumn>{location.coordinates}</TableRowColumn>
-                        <TableRowColumn>{location.category}</TableRowColumn>
-                    </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </MuiThemeProvider>
+  <div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <GridList
+          cols={2}
+          rows={2}
+          padding={2}
+          cellHeight={180}
+          style={styles.gridList}
+          >
+      {locations.map((location, index) => (
+        <GridTile
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          key={index}
+          title={location.name}
+          subtitle={<span>{location.address}</span>}
+          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+          style={styles.gridTile}
+          rows={index%5 == 0 ? 2 : 1}
+          cols={index%5 == 0 ? 2 : 1}
+        >
+          <img  src="src/tools/images/map.jpg" />
+        </GridTile>
+      ))}
+    </GridList>
+    </MuiThemeProvider>
+  </div>
     );
 };
+
 
 LocationList.propTypes = {
     locations: PropTypes.array.isRequired
